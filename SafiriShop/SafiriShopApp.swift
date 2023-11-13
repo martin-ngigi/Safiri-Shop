a9e9abe6c13ad9440f0921d34ef921a596e1c8d8
martin-ngigi <martinwainaina001@gmail.com>
2023-11-13 11:17:55 +0300

    Initial commit

---
+++ SafiriShop/SafiriShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  SafiriShopApp.swift
+//  SafiriShop
+//
+//  Created by Martin Wainaina on 13/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct SafiriShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            WelcomeView()
+        }
+    }
+}
---
+++ SafiriShop/View/Login/WelcomeView.swift
@@ -0,0 +1,20 @@
+//
+//  WelcomeView.swift
+//  SafiriShop
+//
+//  Created by Martin Wainaina on 13/11/2023.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WelcomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        WelcomeView()
+    }
+}
